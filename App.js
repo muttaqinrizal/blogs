c3332e8f3dceabc484456ecea7b9dfe94d7c8e4f
muttaqinrizal <rizalm57@gmail.com>
2020-01-07 11:30:42 +0700

    add create screen

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
 import React from "react";
-import { BlogProvider } from "./src/context/BlogContext";
+import { Provider } from "./src/context/BlogContext";
+import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
 
 const navigator = createStackNavigator(
   {
-    Index: IndexScreen
+    Index: IndexScreen,
+    Show: ShowScreen,
+    Create: CreateScreen
   },
   {
     initialRouteName: "Index",
@@ -20,8 +24,8 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext();
-
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
-
-  const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` }
-    ]);
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`
+        }
+      ];
+    case "delete_blogpost":
+      return state.filter(blogPost => blogPost.id !== action.payload);
+    default:
+      return state;
+  }
+};
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: "add_blogpost" });
   };
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
 };
 
-export default BlogContext;
+const deleteBlogPost = dispatch => {
+  return id => {
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost },
+  []
+);
